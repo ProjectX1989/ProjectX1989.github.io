0f1f1d087df385166e556480cd6cf6cbb4ce1d9b
Alec Larsen <alec.projectx.larsen@gmail.com>
2020-05-20 17:00:01 +0200

    React PWA

---
+++ ReactPWA/precache-manifest.5b58eba5815d3b201d85006f6a285545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e059d01d150e4ac22e5240a5fc41f7fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7edc97ac16a51a61343",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53ea2654beb6fd754a79",
+    "url": "/static/js/2.5119bc39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5119bc39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7edc97ac16a51a61343",
+    "url": "/static/js/main.9573f110.chunk.js"
+  },
+  {
+    "revision": "ab7d2e0a75360113bc06",
+    "url": "/static/js/runtime-main.e46617c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
